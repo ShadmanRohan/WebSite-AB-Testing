c0cf3ca98c5130d22b88e9d26db8b79df5874056
Shadman Rohan <shadmanrohan@gmail.com>
2023-02-21 00:17:33 +0600

    updated

---
+++ starter.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+import itertools
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.stats.api as sms
+from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, pearsonr, spearmanr, kendalltau, \
+    f_oneway, kruskal
+from statsmodels.stats.proportion import proportions_ztest
+
+
+df = pd.read_csv("grocerywebsiteabtestdata.csv")
